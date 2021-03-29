532d1dd7c7bdfe260e4a6148d9eb9790baae0850
SaiidZafari <saiid.zafari@bahnhof.se>
2021-03-29 15:11:25 +0200

    Add Preparation for Database

---
+++ EcChocolateAsp/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcChocolateAsp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Product> Products { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+    }
+}
