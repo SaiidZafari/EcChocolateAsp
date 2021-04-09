31d7154452ba71985d9a6f8a1798bfbfd14eeaae
SaiidZafari <saiid.zafari@bahnhof.se>
2021-04-09 23:17:56 +0200

    Login work as plan

---
+++ EcChocolateAsp/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using EcChocolateAsp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcChocolateAsp.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Add UserManager for async-CRUD ... and SignInManager for signIn signOut ...
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public readonly SignInManager<IdentityUser> signInManager;
+        public readonly UserManager<IdentityUser> userManager;
+
+        // never use HttpGet, due to it can use as a backdoor for a malicious user(hackers) 
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                                
+                    ModelState.AddModelError(string.Empty, "Invaliv Login attempt");
+                
+            }
+            return View(model);
+        }
+    }
+}
