37e6f9b7f6f36e22fbda53eb6a79d621ac7a5b6d
Mikael Wiik <psythrone@gmail.com>
2021-04-12 18:15:10 +0200

    Minor changes to favourites

--- EcChocolateAsp/Models/Favorite.cs
+++ EcChocolateAsp/Models/Favorite.cs
@@ -20,7 +20,8 @@ namespace EcChocolateAsp.Models
                 {
                     Id = product.Id,
                     Name = product.Name,
-                    Price = product.Price
+                    Price = product.Price,
+                    Image = product.Image
                 });
             }
         }
@@ -32,6 +33,7 @@ namespace EcChocolateAsp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public string Image { get; set; }
     }
 
 }
